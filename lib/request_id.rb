d4fdbf6ac75663b545b7f66b2e4d99b9aa9389fe
maiha <maiha@wota.jp>
2008-11-17 18:47:40 +0900

    add ActionController::Base#backend_port

--- lib/request_id.rb
+++ lib/request_id.rb
@@ -1,6 +1,6 @@
 module RequestId
   def self.included(base)
-    instance_methods = rails22?(base) ? Rails22 : Rails21
+    instance_methods = rails2x? ? Rails2x : Rails1x
     base.class_eval do
       cattr_accessor :max_request_id
       cattr_accessor :request_id_with_port
@@ -9,11 +9,38 @@ module RequestId
     end
   end
 
-  def self.rails22?(base)
-    base.instance_methods.include?("log_processing_for_request_id")
+  def self.rails2x?
+    ActionController::Base.private_instance_methods.include?("log_processing_for_request_id")
   end
 
-  module Rails21
+  def request_id
+    @request_id ||= new_request_id
+  end
+
+  private
+    def new_request_id
+      id = self.class.max_request_id = self.class.max_request_id.to_i + 1
+      if self.class.request_id_with_port
+        port = backend_port || request.port
+        "#{port}:#{id}"
+      else
+        id.to_s
+      end
+    end
+
+    def backend_port
+      case request.cgi.class.name
+      when "Mongrel::CGIWrapper"
+        request.cgi.handler.listener.port rescue "unknown_mongrel"
+      when "Rack::Adapter::Rails::CGIWrapper"
+        # not implemented yet
+        nil
+      else
+        nil
+      end
+    end
+
+  module Rails1x
     def self.included(base)
       base.class_eval do
         alias_method_chain :log_processing, :request_id
@@ -21,19 +48,6 @@ module RequestId
     end
 
     private
-      def request_id
-        @request_id ||= new_request_id
-      end
-
-      def new_request_id
-        id = self.class.max_request_id = self.class.max_request_id.to_i + 1
-        if self.class.request_id_with_port
-          "#{request.port}:#{id}"
-        else
-          id.to_s
-        end
-      end
-
       def log_processing_with_request_id
         if logger
           logger.info "\n\nProcessing #{controller_class_name}\##{action_name} (for #{request_origin}) [#{request.method.to_s.upcase}]"
@@ -44,7 +58,7 @@ module RequestId
       end
   end
 
-  module Rails22
+  module Rails2x
     def self.included(base)
       base.class_eval do
         alias_method_chain :log_processing_for_request_id, :request_id
@@ -54,7 +68,7 @@ module RequestId
     private
       def log_processing_for_request_id_with_request_id
         log_processing_for_request_id_without_request_id
-        logger.info "  Request ID: #{request_id} (port:#{request.port})"
+        logger.info "  Request ID: #{request_id}"
       end
   end
 end
